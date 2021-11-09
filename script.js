e1ef61c1b0b58e6768ba0d7cd2b67777bd02104f
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-11-09 11:28:00 +0100

    struture and styling completed

---
+++ script.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById("FName")
+const lastName = document.getElementById("lName")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
