a5bc5fabf48cf511ec33a37969fd8bfd4ae5eb22
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-11-09 21:51:30 +0100

    update project - create javaScript file - update index and css file

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const firstName = document.getElementById("FName")
+const form = document.getElementById("form")
+const firstName = document.getElementById("fName")
 const lastName = document.getElementById("lName")
 const email = document.getElementById("email")
-const password = document.getElementById("password")
+// const password = document.getElementById("password")
+const errorMessage = document.querySelector('span')
 
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    checkInput()
+
+})
+function checkInput() {
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const emailValue = email.value;
+
+    if (firstNameValue === "") {
+        errorMessage.textContent = "first name cannot be empty"
+        firstName.style.borderColor = "#d44646";
+    } else {
+        firstName.style.borderColor = "green"
+    }
+    if (lastNameValue === "") {
+        errorMessage.textContent = "last Name cannot be empty"
+    } else {
+        lastName.style.borderColor = "green"
+    }
+    if (emailValue === "") {
+        errorMessage.textContent = "Looks like this is not email"
+    } else {
+        
+
+    }
+}
